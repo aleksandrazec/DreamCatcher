fb08f104b43aaf7a4d8804a6ac8602b0d59a4cbc
Aleksandra Zec <sanjazec04@gmail.com>
2026-04-19 23:34:52 +0200

    l-shaped rotations are corrected

--- Assets/Scripts/MapGeneration/Cell.cs
+++ Assets/Scripts/MapGeneration/Cell.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +11,28 @@ public class Cell : MonoBehaviour
     public int columnIndex;
     public int value;
     public Image image;
-    public void SetSpecialRoomSprite(Sprite icon)
+    public CellType typeOfCell=CellType.single;
+    [SerializeField] public RectTransform rectTransform;
+    public enum CellType{
+        single,
+        big0,
+        big1,
+        big2,
+        big3,
+        horizontal0,
+        horizontal1,
+        vertical0,
+        vertical1,
+        lshape0,
+        lshape1,
+        lshape2
+    }
+    public void SetRoomSprite(Sprite icon)
     {
         image.sprite = icon;
     }
-
+    public void RotateSprite(float angle)
+    {
+        rectTransform.Rotate(new Vector3(0, 0, angle));
+    }
 }
--- Assets/Scripts/MapGeneration/MapGenerator.cs
+++ Assets/Scripts/MapGeneration/MapGenerator.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Linq;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -13,7 +15,7 @@ public class MapGenerator : MonoBehaviour
     private int floorPlanCount;
     private int minRooms;
     private int maxRooms;
-    private List<(int,int)> endRooms;
+    private List<(int, int)> endRooms;
 
     private (int, int) bossRoomIndex;
     private (int, int) shopRoomIndex;
@@ -21,18 +23,64 @@ public class MapGenerator : MonoBehaviour
 
     public Cell cellPrefab;
     public GameObject emptyCellPrefab;
-    private Queue<(int,int)> cellQueue;
+    private Queue<(int, int)> cellQueue;
     private List<Cell> spawnedCells;
     private List<GameObject> spawnedEmpties;
 
     [SerializeField] private Canvas canvas;
+
+    private List<(int, int)> bigRoomIndexes;
+    private List<List<(int, int)>> bigRooms;
 
     [Header("Sprite References")]
     [SerializeField] private Sprite item;
     [SerializeField] private Sprite shop;
     [SerializeField] private Sprite boss;
 
+    [Header("Room Variations")]
+    [SerializeField] private Sprite big0;
+    [SerializeField] private Sprite big1;
+    [SerializeField] private Sprite big2;
+    [SerializeField] private Sprite big3;
+    [SerializeField] private Sprite vertical0;
+    [SerializeField] private Sprite vertical1;
+    [SerializeField] private Sprite horizontal0;
+    [SerializeField] private Sprite horizontal1;
+    [SerializeField] private Sprite lShape0;
+    [SerializeField] private Sprite lShape1;
+    [SerializeField] private Sprite lShape2;
+
     private InputSystem_Actions _inputActions;
+
+    private static readonly List<(int, int)[]> roomShapes = new() {
+        new (int, int)[] { (0,-1) },
+        new (int, int)[] { (0,1) },
+
+        new (int, int)[] { (1,0) },
+        new (int, int)[] { (-1,0) },
+
+        new (int, int)[] { (0,1), (1,0) },
+        new (int, int)[] { (0,1), (1,1) },
+        new (int, int)[] { (1,0), (1,1) },
+
+        new (int, int)[] { (1,-1), (1,0) },
+        new (int, int)[] { (0,-1), (1,-1) },
+        new (int, int)[] { (0,-1), (1,0) },
+
+        new (int, int)[] { (0,1), (-1,0) },
+        new (int, int)[] { (0,1), (-1,1) },
+        new (int, int)[] { (-1,1), (-1,0) },
+
+        new (int, int)[] { (0,-1), (-1,0) },
+        new (int, int)[] { (0,-1), (-1,-1) },
+        new (int, int)[] { (-1,0), (-1,-1) },
+
+        new (int, int)[] { (0,1), (1,0), (1,1) },
+        new (int, int)[] { (0,1), (-1,1), (-1,0) },
+        new (int, int)[] { (0,-1), (1,-1), (1,0) },
+        new (int, int)[] { (0,-1), (-1,0), (-1,-1) },
+    };
+
     private void Awake()
     {
         _inputActions = new InputSystem_Actions();
@@ -50,7 +98,7 @@ public class MapGenerator : MonoBehaviour
     }
     private void GenerateMap(InputAction.CallbackContext obj)
     {
-       SetupDungeon();
+        SetupDungeon();
     }
     void Start()
     {
@@ -79,33 +127,37 @@ public class MapGenerator : MonoBehaviour
         floorPlanCount = 0;
         cellQueue = new Queue<(int, int)>();
         endRooms = new List<(int, int)>();
+        bigRoomIndexes = new List<(int, int)>();
+        bigRooms = new List<List<(int, int)>>();
 
         VisitCell(4, 5);
         GenerateDungeon();
-    } 
+    }
     void GenerateDungeon()
     {
         while (cellQueue.Count > 0)
         {
-            (int,int) index= cellQueue.Dequeue();
+            (int, int) index = cellQueue.Dequeue();
             int rowIndex = index.Item1;
             int columnIndex = index.Item2;
             bool created = false;
             if (columnIndex > 1) created |= VisitCell(rowIndex, columnIndex - 1);
-            if(columnIndex < 9) created |= VisitCell(rowIndex, columnIndex + 1);
+            if (columnIndex < 9) created |= VisitCell(rowIndex, columnIndex + 1);
             if (rowIndex > 2) created |= VisitCell(rowIndex - 1, columnIndex);
-            if (rowIndex < 7) created|= VisitCell(rowIndex + 1, columnIndex);
+            if (rowIndex < 7) created |= VisitCell(rowIndex + 1, columnIndex);
 
             if (created == false)
             {
                 endRooms.Add((rowIndex, columnIndex));
             }
         }
-        if (floorPlanCount< minRooms)
+        if (floorPlanCount < minRooms)
         {
             SetupDungeon();
             return;
         }
+        CleanEndRoomsList();
+        SetUpVisuals();
         SetupSpecialRooms();
     }
     void SetupSpecialRooms()
@@ -122,32 +174,36 @@ public class MapGenerator : MonoBehaviour
             SetupDungeon();
             return;
         }
-        SetUpVisuals();
         UpdateSpecialRoomVisuals();
 
     }
+    void CleanEndRoomsList()
+    {
+        endRooms.RemoveAll(item => bigRoomIndexes.Contains(item) || GetNeighbourCount(item.Item1, item.Item2) > 1);
+    }
+
     void UpdateSpecialRoomVisuals()
     {
         foreach (var cell in spawnedCells)
         {
             if ((cell.rowIndex, cell.columnIndex) == itemRoomIndex)
             {
-                cell.SetSpecialRoomSprite(item);
+                cell.SetRoomSprite(item);
             }
             if ((cell.rowIndex, cell.columnIndex) == shopRoomIndex)
             {
-                cell.SetSpecialRoomSprite(shop);
+                cell.SetRoomSprite(shop);
             }
             if ((cell.rowIndex, cell.columnIndex) == bossRoomIndex)
             {
-                cell.SetSpecialRoomSprite(boss);
+                cell.SetRoomSprite(boss);
             }
         }
     }
-    (int,int) RandomEndRoom()
+    (int, int) RandomEndRoom()
     {
-        if(endRooms.Count == 0) return (-1,-1);
-        int randomRoom = UnityEngine.Random.Range(0,endRooms.Count);
+        if (endRooms.Count == 0) return (-1, -1);
+        int randomRoom = UnityEngine.Random.Range(0, endRooms.Count);
         (int, int) index = endRooms[randomRoom];
         endRooms.RemoveAt(randomRoom);
         return index;
@@ -159,10 +215,10 @@ public class MapGenerator : MonoBehaviour
         int right = 0;
         int up = 0;
         try { down = floorPlan[rowIndex - 1, columnIndex]; } catch { }
-        try { up= floorPlan[rowIndex + 1, columnIndex]; } catch { }
+        try { up = floorPlan[rowIndex + 1, columnIndex]; } catch { }
         try { left = floorPlan[rowIndex, columnIndex - 1]; } catch { }
         try { right = floorPlan[rowIndex, columnIndex + 1]; } catch { }
-        return left+right+up+down;
+        return left + right + up + down;
     }
     private bool VisitCell(int rowIndex, int columnIndex)
     {
@@ -170,11 +226,74 @@ public class MapGenerator : MonoBehaviour
         {
             return false;
         }
-        cellQueue.Enqueue((rowIndex,columnIndex));
+        if (Random.value < 0.3f && !(rowIndex == 4 && columnIndex == 5))
+        {
+            foreach (var shape in roomShapes.OrderBy(_ => UnityEngine.Random.value))
+            {
+                if (TryPlaceRoom(rowIndex, columnIndex, shape))
+                {
+                    return true;
+                }
+            }
+        }
+        cellQueue.Enqueue((rowIndex, columnIndex));
         floorPlan[rowIndex, columnIndex] = 1;
         floorPlanCount++;
         return true;
     }
+    private bool TryPlaceRoom(int rowOrigin, int columnOrigin, (int, int)[] offsets)
+    {
+        List<(int, int)> currentRoomIndexes = new List<(int, int)>() { (rowOrigin, columnOrigin) };
+        foreach (var offset in offsets)
+        {
+            int currentRowIndexChecked = rowOrigin + offset.Item1;
+            int currentColumnIndexChecked = columnOrigin + offset.Item2;
+
+            if (currentRowIndexChecked - 1 < 0 || currentRowIndexChecked + 1 >= floorPlan.Length)
+            {
+                return false;
+            }
+            if (currentColumnIndexChecked - 1 < 0 || currentColumnIndexChecked + 1 >= floorPlan.Length)
+            {
+                return false;
+            }
+            try
+            {
+                if (floorPlan[currentRowIndexChecked, currentColumnIndexChecked] != 0)
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            if (currentRowIndexChecked == rowOrigin && currentColumnIndexChecked == columnOrigin) continue;
+            if (currentColumnIndexChecked == 0) continue;
+
+            currentRoomIndexes.Add((currentRowIndexChecked, currentColumnIndexChecked));
+
+        }
+        if (currentRoomIndexes.Count == 1) return false;
+
+        var newList = new List<(int, int)>();
+
+        foreach ((int, int) index in currentRoomIndexes)
+        {
+            floorPlan[index.Item1, index.Item2] = 1;
+            floorPlanCount++;
+            cellQueue.Enqueue(index);
+
+            bigRoomIndexes.Add(index);
+            newList.Add(index);
+        }
+        bigRooms.Add(newList);
+
+        return true;
+    }
+
+
     private void SpawnEmpty()
     {
         GameObject newEmpty = Instantiate(emptyCellPrefab, this.transform);
@@ -183,20 +302,173 @@ public class MapGenerator : MonoBehaviour
     private void SpawnRoom(int rowIndex, int columnIndex)
     {
         Cell newCell = Instantiate(cellPrefab, this.transform);
+        if (bigRoomIndexes.Contains((rowIndex, columnIndex)))
+        {
+            List<(int, int)> temp = null;
+            foreach (List<(int, int)> indexesList in bigRooms)
+            {
+                if (indexesList.Contains((rowIndex, columnIndex)))
+                {
+                    temp = indexesList;
+                    break;
+                }
+            }
+            switch (temp.Count)
+            {
+                case 4:
+                    switch (temp.IndexOf((rowIndex, columnIndex)))
+                    {
+                        case 0:
+                            newCell.typeOfCell = Cell.CellType.big0;
+                            break;
+                        case 1:
+                            newCell.typeOfCell = Cell.CellType.big1;
+                            break;
+                        case 2:
+                            newCell.typeOfCell = Cell.CellType.big2;
+                            break;
+                        case 3:
+                            newCell.typeOfCell = Cell.CellType.big3;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 3:
+                    Debug.Log(temp[0]);
+                    Debug.Log(temp[1]);
+                    Debug.Log(temp[2]);
+                    if (temp[1] == (temp[0].Item1 + 1, temp[0].Item2) && temp[2] == (temp[0].Item1 + 1, temp[0].Item2 + 1))
+                    {
+                        Debug.Log("case "+0);
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.lshape0;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.lshape1;
+                                break;
+                            case 2:
+                                newCell.typeOfCell = Cell.CellType.lshape2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }else if (temp[1] == (temp[0].Item1, temp[0].Item2+1) && temp[2] == (temp[0].Item1 + 1, temp[0].Item2))
+                    {
+                        Debug.Log("case " + 1);
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.lshape1;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.lshape0;
+                                break;
+                            case 2:
+                                newCell.typeOfCell = Cell.CellType.lshape2;
+                                break;
+                            default:
+                                break;
+                        }
+                        newCell.RotateSprite(-90);
+                    }else if (temp[1] == (temp[0].Item1, temp[0].Item2 + 1) && temp[2] == (temp[0].Item1 + 1, temp[0].Item2+1))
+                    {
+                        Debug.Log("case " + 2);
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.lshape2;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.lshape1;
+                                break;
+                            case 2:
+                                newCell.typeOfCell = Cell.CellType.lshape0;
+                                break;
+                            default:
+                                break;
+                        }
+                        newCell.RotateSprite(-180);
+                    }else
+                    {
+                        Debug.Log("case " + 3);
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.lshape2;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.lshape0;
+                                break;
+                            case 2:
+                                newCell.typeOfCell = Cell.CellType.lshape1;
+                                break;
+                            default:
+                                break;
+                        }
+                        newCell.RotateSprite(-270);
+                    }
+
+                    break;
+                case 2:
+                    if (temp[0].Item1 + 1 == temp[1].Item1 || temp[0].Item1 - 1 == temp[1].Item1)
+                    {
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.vertical0;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.vertical1;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        switch (temp.IndexOf((rowIndex, columnIndex)))
+                        {
+                            case 0:
+                                newCell.typeOfCell = Cell.CellType.horizontal0;
+                                break;
+                            case 1:
+                                newCell.typeOfCell = Cell.CellType.horizontal1;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+                default: break;
+            }
+        }
+
         newCell.value = 1;
-        newCell.rowIndex= rowIndex;
-        newCell.columnIndex= columnIndex;
+        newCell.rowIndex = rowIndex;
+        newCell.columnIndex = columnIndex;
 
         spawnedCells.Add(newCell);
     }
     private void SetUpVisuals()
     {
-        int[] emptyArray= new int[numOfRows];
+        foreach (List<(int, int)> bigRoom in bigRooms)
+        {
+            bigRoom.Sort((x, y) =>
+            {
+                int result = x.Item1.CompareTo(y.Item1);
+                return result == 0 ? x.Item2.CompareTo(y.Item2) : result;
+            });
+        }
+
+        int[] emptyArray = new int[numOfRows];
         for (int i = 0; i < floorPlan.GetLength(0); i++)
         {
             for (int j = 0; j < floorPlan.GetLength(1); j++)
             {
-                emptyArray[i]|=floorPlan[i,j];
+                emptyArray[i] |= floorPlan[i, j];
             }
         }
         for (int i = 0; i < floorPlan.GetLength(0); i++)
@@ -207,12 +479,59 @@ public class MapGenerator : MonoBehaviour
             }
             for (int j = 0; j < floorPlan.GetLength(1); j++)
             {
-                if(floorPlan[i, j] != 0) {
+                if (floorPlan[i, j] != 0)
+                {
                     SpawnRoom(i, j);
-                }else 
+                }
+                else
                 {
                     SpawnEmpty();
                 }
+            }
+        }
+        UpdateLargeRoomVisuals();
+    }
+    void UpdateLargeRoomVisuals()
+    {
+        foreach (var cell in spawnedCells)
+        {
+            switch (cell.typeOfCell)
+            {
+                case Cell.CellType.big0:
+                    cell.SetRoomSprite(big0);
+                    break;
+                case Cell.CellType.big1:
+                    cell.SetRoomSprite(big1);
+                    break;
+                case Cell.CellType.big2:
+                    cell.SetRoomSprite(big2);
+                    break;
+                case Cell.CellType.big3:
+                    cell.SetRoomSprite(big3);
+                    break;
+                case Cell.CellType.lshape0:
+                    cell.SetRoomSprite(lShape0);
+                    break;
+                case Cell.CellType.lshape1:
+                    cell.SetRoomSprite(lShape1);
+                    break;
+                case Cell.CellType.lshape2:
+                    cell.SetRoomSprite(lShape2);
+                    break;
+                case Cell.CellType.horizontal0:
+                    cell.SetRoomSprite(horizontal0);
+                    break;
+                case Cell.CellType.horizontal1:
+                    cell.SetRoomSprite(horizontal1);
+                    break;
+                case Cell.CellType.vertical0:
+                    cell.SetRoomSprite(vertical0);
+                    break;
+                case Cell.CellType.vertical1:
+                    cell.SetRoomSprite(vertical1);
+                    break;
+                default:
+                    break;
             }
         }
     }
